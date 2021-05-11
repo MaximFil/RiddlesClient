fae2651b705c346c500ad2785c32d673731d9db3
MaximFil <maks1998mops@mail.ru>
2021-05-11 21:19:55 +0300

    fixed invite size

--- Riddles/Invite.Designer.cs
+++ Riddles/Invite.Designer.cs
@@ -74,7 +74,7 @@ namespace Riddles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(335, 169);
+            this.ClientSize = new System.Drawing.Size(316, 169);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
